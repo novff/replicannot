27fc0f2bb761f94a73598d0eb64bbbc1f37ab298
novff <max20042011@gmail.com>
2024-05-31 00:42:22 +0000

    automatic backup exec commit

--- backup.sh
+++ backup.sh
@@ -1,25 +1,31 @@
 cwd=$(pwd)
 df=$cwd/configs/dotfiles
 
-#extract in-repo packages
-pacman -Qn |cut -f 1 -d " "  > ./pkg.txt
-
+#extract arch-repo packages
+	pacman -Qn |cut -f 1 -d " "  > ./pkg.txt
 #extract aur packages
-pacman -Qm |cut -f 1 -d " " > ./aur.txt
-
+	pacman -Qm |cut -f 1 -d " " > ./aur.txt
+#extract flatpaks
+	#flatpak list --columns=application > ./flat.txt
+	flatpak-sync generate -v
+	cp -r ~/.config/flatpak-sync/flatpak.json ./flatpack.json 
+#extract snaps
+	#how doe
 #extract gnome extensions
 #gnome-extensions list -d> ./gnomeext.txt
-cp -r ~/.local/share/gnome-shell/extensions/ ./extensions
+	cp -r ~/.local/share/gnome-shell/extensions/ ./extensions
 
 #extract settings for desktop via dconf
-dconf dump / > dconf.txt
+	dconf dump / > dconf.txt
 
 #backup dotfiles and configurations
-cp ~/.zshrc $df
-cp -r ~/.themes $df
-cp -r ~/.icons $df
-cp -r ~/Templates $cwd/configs/Templates
+	cp ~/.zshrc $df
+	cp -r ~/.themes $df
+	cp -r ~/.icons $df
+	cp -r ~/Templates $cwd/configs/Templates
 
-
-git add .
+#github automation
+	git add .
+	git commit -m "automatic backup exec commit"
+	#git push origin
 
--- replicate.sh
+++ replicate.sh
@@ -1,30 +1,30 @@
 cwd=$(pwd)
 df=$cwd/configs/dotfiles
 
-#install packages from arch repo which are explicitly found in repo
-sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo pacman -S --noconfirm --needed - < packages.txt
-
-
-
-#install yay-bin
-git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin
-makepkg -si
-cd ..
-rm -rf yay-bin
-
-#install packages from yay
-yay -S $(cat aur.txt)
-
-#emplace gnome extensions
-cp -r $df/extensions ~/.local/share/gnome-shell/extensions/
-
-#restore settings from dconf
-dconf load / < dconf.txt
-
 #emplace dotfiles and configs
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp $df/.zshrc ~/
-cp -r $df/.themes ~/
-cp -r $df/.icons ~/
-cp -r $cwd/Templates ~/Templates
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	cp $df/.zshrc ~/
+	cp -r $df/.themes ~/
+	cp -r $df/.icons ~/
+	cp -r $cwd/Templates ~/Templates
+	cp -r $cwd/flatpack.json ~/.config/flatpak-sync/flatpak.json
+#install packages from arch repo which are explicitly found in repo
+	sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+	sudo pacman -S --noconfirm --needed - < packages.txt
+#install yay-bin
+	git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin
+	makepkg -si
+	cd ..
+	rm -rf yay-bin
+#install packages from yay
+	yay -S --quiet --noconfirm --sudoloop $(cat aur.txt)
+#install flatpak and flatpak-sync with its deps
+	sudo pacman -S --noconfirm --needed flatpak python python-pip
+	pip install --user flatpak-sync --break-system-packages
+#install flatpak packages
+	flatpak-sync run -v
+#emplace gnome extensions
+	cp -r $df/extensions ~/.local/share/gnome-shell/extensions/
+#restore settings from dconf
+	dconf load / < dconf.txt
+
